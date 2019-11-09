b080cebcf9347d8285543acb5cb6b88d1a3c0446
Jackson Jewell <jackson.t.jewell@gmail.com>
2019-11-09 13:54:51 -0600

    Project completed

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # password-gen
 
 This program generates a random password at the click of a button.
+
+A screenshot of the completed project is in the assets folder.
--- assets/javascript.js
+++ assets/javascript.js
@@ -7,13 +7,13 @@ function genPassword(){
     var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var numbers = ["1","2","3","4","5","6","7","8","9","0"];
-    var textWord = .document.body.children[1];
+    var pass = document.getElementById("pass")
 
     //generate a random number between 8 and 128 characters
     length = numGen(8,128);
 
     //until i = length
-    for(var i < length){
+    for(i = 0; i < length;){
 
         //generate a random number between one and 4
         newChar = numGen(1,4);
@@ -25,6 +25,7 @@ function genPassword(){
             newChar = specChars[picker];
             //add the character to the password variable
             password = password + newChar;
+            
             i++;
         }else if(newChar === 2){
             //for letters generate a random number between 1 and 26
@@ -48,13 +49,18 @@ function genPassword(){
     }
     
     //assign the newly generated password to the HTMl
-    textWord = password;
+    pass.value = password;
 }
 
 function copy(){
     //copy the password to the clipboard
+    var copyText = document.getElementById("pass");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
 }
 
 function numGen(min,max){
     //generate some numbers
+    return (Math.floor(Math.random() * (max - min + 1))+min);
 }
